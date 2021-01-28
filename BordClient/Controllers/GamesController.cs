89526a5ab7cdfdbdee8dac7f4afd93bd9135e987
kevinkirkl3y <kevinmkirkley@gmail.com>
2021-01-28 10:58:06 -0800

    add to README.
    Co-authored-by: Nataliya Zhuravleva<natalindria@gmail.com>

--- BordClient/Controllers/GamesController.cs
+++ BordClient/Controllers/GamesController.cs
@@ -78,28 +78,28 @@ namespace BordClient.Controllers
             return RedirectToAction("Index");
         }
 
-    public ActionResult AddGenre(int id)
-    {
-        Genre thisGenre = _db.Genres.FirstOrDefault(genre => genre.GenreId == id);
-        ViewBag.GenreId = new SelectList(_db.Genres, "GenreId", "GenreName");
-        return View(thisGenre);
-    }
+    // public ActionResult AddGenre(int id)
+    // {
+    //     Genre thisGenre = _db.Genres.FirstOrDefault(genre => genre.GenreId == id);
+    //     ViewBag.GenreId = new SelectList(_db.Genres, "GenreId", "GenreName");
+    //     return View(thisGenre);
+    // }
 
-    [HttpPost]
-    public ActionResult AddGenre(Flavor flavor, int GenreId)
-    {
-        if (GenreId != 0)
-        {
-            var returnedJoin = _db.GenreFlavor
-            .Any(join => join.FlavorId == flavor.FlavorId && join.GenreId == GenreId);
-            if (!returnedJoin)
-            {
-            _db.GenreFlavor.Add(new GenreFlavor() { FlavorId = flavor.FlavorId, GenreId=GenreId});
-            }
-        }
-        _db.SaveChanges();
-        return RedirectToAction("Details", "Flavors", new { id = flavor.FlavorId });
-    }
+    // [HttpPost]
+    // public ActionResult AddGenre(Flavor flavor, int GenreId)
+    // {
+    //     if (GenreId != 0)
+    //     {
+    //         var returnedJoin = _db.GenreFlavor
+    //         .Any(join => join.FlavorId == flavor.FlavorId && join.GenreId == GenreId);
+    //         if (!returnedJoin)
+    //         {
+    //         _db.GenreFlavor.Add(new GenreFlavor() { FlavorId = flavor.FlavorId, GenreId=GenreId});
+    //         }
+    //     }
+    //     _db.SaveChanges();
+    //     return RedirectToAction("Details", "Flavors", new { id = flavor.FlavorId });
+    // }
     
         public IActionResult Create()
         {
